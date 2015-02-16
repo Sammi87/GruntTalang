0c978ba001ccc44d2de7572051bd7b4753648239
Sammi Haj Hassine <sammi.haj.hassine@valtech.se>
2015-02-16 09:15:06 +0100

    Added a working Gruntfile and a updated package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,50 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    templatePath: 'assets/javascripts/src/views/templates',
+    outputPath: 'assets/javascripts/src/views/templates.js',
+    jshint: {
+      options: {
+        jshintrc: true,
+        ignores: [
+                 'assets/javascripts/src/components/**/*.js'
+                 ]
+      },
+      target: ['assets/javascripts/'],
+    },
+    sass: {
+      dist: {
+        files: {
+          'dist/build/styles/application.css': 'assets/styles/application.scss'
+        }
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'dist/build/scripts/application.js': [
+            'assets/javascripts/src/Game.js'
+          ]
+        }
+      }
+    },
+    watch: {
+      options : {
+        livereload: true
+      },
+      files: ['assets/javascripts/src/**/*.js',
+              'assets/styles/**/*.scss'],
+      tasks: ['dev']
+    }
+  });
 
-    });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // load tasks
-    // task definitions
+  grunt.registerTask('dev',
+                     ['jshint', 'sass', 'browserify']);
 
-  };
+  grunt.registerTask('build',
+                     ['jshint', 'sass', 'browserify']);
+};
